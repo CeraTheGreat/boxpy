9cf174727c2d264a06fbeadefe4e610942e228f1
CeraTheGreat <csr0897@gmail.com>
2019-07-11 19:40:34 -0500

    oauth credentials no longer hardcoded

---
+++ cred/README.md
@@ -0,0 +1,9 @@
+#Credentials
+credentials go in a json file named `cred.json` in this folder
+
+```json
+{
+    "client_id":"<client_id>",
+    "client_secret":"<client_secret>"
+}
+```
--- src/oauth.py
+++ src/oauth.py
@@ -2,14 +2,18 @@ from boxsdk import OAuth2
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import webbrowser
 import requests
+import json
 
 auth_code = None
 
 class BoxOAuth:
     def __init__(self):
-    
-        self.client_id = 'ba6xqqqso7wauppnvtixr258oemo1cnk'
-        self.client_secret ='jB1t76Q4WlnRL8ETYwI2gc2Doa90Rrkm'
+
+        cred_file = open('cred/cred.json',"r")
+        cred_json = json.load(cred_file)
+
+        self.client_id = cred_json[client_id]
+        self.client_secret = cred_json[cred_json]
         self.redirect_port = 55655
         self.redirect_uri = 'http://localhost:{}/'.format(self.redirect_port)
 
@@ -20,16 +24,16 @@ class BoxOAuth:
 
     def _store_tokens(self, access_token, refresh_token):
         self.token_dict['access_token'],self.token_dict['refresh_token'] = access_token,refresh_token
-        
+
     def logout(self):
         data = {"client_id":self.client_id,"client_secret":self.client_secret,"token":self.token_dict['refresh_token'],"token":self.token_dict['access_token']}
-        return requests.post('https://api.box.com/oauth2/revoke', json=data)    
-        
+        return requests.post('https://api.box.com/oauth2/revoke', json=data)
+
     def dev_login(self, token):
         """
         Developer login, must use developer token generated from the
         Box application administration page.
-        
+
             :param token: the developer token
             :type token: string
         """
@@ -38,12 +42,12 @@ class BoxOAuth:
             client_secret=self.client_secret,
             access_token=token
         )
-        
+
     def user_login(self):
         """
         User login, uses OAuth2
         """
-        
+
         #auth_code is a global variable set by the server when it exits
         global auth_code
 
@@ -63,14 +67,14 @@ class BoxOAuth:
 
         #wait for the redirect
         httpd.serve_forever()
-        
+
         if auth_code is None:
             raise Exception("local server unable to retrieve auth code")
         if 'state' not in auth_code:
             raise Exception("Invalid URL")
 
         csrf_token = auth_code['state'][0]
-        
+
         #double check that we have the right session
         try:
             assert csrf_token == self.csrf_token
@@ -115,11 +119,10 @@ class HTTPLoopbackHandler(BaseHTTPRequestHandler):
         self.wfile.write(b'Login Successful!')
 
         auth_code = parse_qs(urlparse(self.path).query)
-        
+
         #kill this server after we respond to this
         self.force_stop()
 
     def log_message(self, format, *args):
         #we don't care about log messages
         pass
-
